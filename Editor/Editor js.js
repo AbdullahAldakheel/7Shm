370677d659eb40ff81c944ae30ce61e1129c0f4a
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2018-11-20 15:35:52 +0300

    Aziz

--- Editor/Editor js.js
+++ Editor/Editor js.js
@@ -1,19 +1,23 @@
-$(document).ready(function(){
-    $(".area").hide();
-    $(".cancel").hide();
-    $(".sub").hide();
-$(".com").click(function(){
-    $(".cancel").show();
-    $(".area").show();
-    $(".sub").show();
-    $(".com").hide();
-    $(".app").hide();
-});             
-$(".cancel").click(function(){
-    $(".area").hide();
-    $(".cancel").hide();
-    $(".app").show();
-    $(".sub").hide();
-    $(".com").show();
-});
-});
+  $(document).ready(function(){
+             
+              $(".area").hide();
+              $(".cancel").hide();
+             $(".sub").hide();
+             $(".com").click(function(){
+              $(".cancel").show();
+                  $(".area").show();
+                 $(".sub").show();
+                  $(".com").hide();
+                 $(".app").hide();
+             });
+             
+             $(".cancel").click(function(){
+                $(".area").hide();
+              $(".cancel").hide();
+                  $(".app").show();
+                 $(".sub").hide();
+                 $(".com").show();
+                 
+             });
+             $(".can").blur().css("background-color","#cccccc");
+         });
