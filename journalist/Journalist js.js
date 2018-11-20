01d5ecdc09b431b4b785661f79410c6b151afa6b
Abdullah Alhomaidhi <Alhomaidhi97@gmail.com>
2018-11-20 04:25:10 +0300

    AAA

--- journalist/Journalist js.js
+++ journalist/Journalist js.js
@@ -6,7 +6,18 @@ function auto_grow(element) {
 function myFunction() {
     document.getElementById("myP").contentEditable = true;
     document.getElementById("myP1").contentEditable = true;
-    document.getElementById("myb").disabled = false;
+    document.getElementById("myb").hidden = false;
+    document.getElementById("bt3").hidden = false;
+    document.getElementById("bt1").hidden = true;
+    document.getElementById("bt2").hidden = true;
+}
+function myFunction1() {
+    document.getElementById("myP").contentEditable = false;
+    document.getElementById("myP1").contentEditable = false;
+     document.getElementById("myb").hidden = true;
+    document.getElementById("bt3").hidden = true;
+    document.getElementById("bt1").hidden = false;
+    document.getElementById("bt2").hidden = false;
 }
 function clic(element){
  var x = confirm("Are you sure you want to delete");
@@ -18,3 +29,4 @@ function clic(element){
     }
     document.getElementById("demo").innerHTML = txt;
 }
+
